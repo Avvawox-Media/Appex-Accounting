05147555f9c40b0cde19757d9cc87215617df408
Benedict C. Korie <benedictkorie@gmail.com>
2021-03-16 06:42:55 +0100

    User Authentication implemented

--- lib/core/exceptions/exceptions.dart
+++ lib/core/exceptions/exceptions.dart
@@ -1,12 +1,6 @@
 // class SqlException implements DatabaseException {}
 
-class DatabaseException implements Exception {
-  final String _message;
-
-  DatabaseException(this._message);
-
-  String get message => _message;
-}
+class DatabaseException implements Exception {}
 
 class CacheException implements Exception {}
 
